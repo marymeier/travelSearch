2ef1f552f96c758c00485a930d7dc89c9e262b76
Roy Schor <70181314+RoySchor@users.noreply.github.com>
2023-11-27 18:41:43 -0500

    added missing info to the database

--- tourist_guide.py
+++ tourist_guide.py
@@ -8,12 +8,40 @@ def clean_country_list(file_name):
     europe_data = europe_data[europe_data['continent'] == 'Europe']
     selected_columns = ['country', 'currency', 'capital_city', 'region', 'gdp', 'population', 'democracy_type']
     europe_data = europe_data[selected_columns]
-    return europe_data
+
+    # Fixing democracy type on major basis
+    democracy_mapping = {'Flawed democracy': 'Parliamentary democracy', 'Full democracy': 'Parliamentary democracy', 'Hybrid regime': 'Constitutional monarchy'}
+    europe_data['democracy_type'] = europe_data['democracy_type'].replace(democracy_mapping)
+
+    # Fixing democracy type
+    europe_data.loc[europe_data['country'] == 'Turkey', 'democracy_type'] = 'Presidential republic'
+    europe_data.loc[europe_data['country'] == 'Cyprus', 'democracy_type'] = 'Presidential republic'
+    europe_data.loc[europe_data['country'] == 'Monaco', 'democracy_type'] = 'Constitutional monarchy'
+    europe_data.loc[europe_data['country'] == 'San Marino', 'democracy_type'] = 'Parlimentary democracy'
+    europe_data.loc[europe_data['country'] == 'Andorra', 'democracy_type'] = 'Parlimentary democracy'
+    europe_data.loc[europe_data['country'] == 'Liechtenstein', 'democracy_type'] = 'Semi-constitutional monarchy'
+
+    europe_data = europe_data.reset_index(drop=True)
+
+    # Add new Countries:
+    missing_countries_data = {'country': ['Armenia', 'Azerbaijan', 'Georgia', 'Kosovo', 'Turkey', 'Vatican City'],
+                      'currency': ['Armenian Dram', 'Azerbaijani Manat', 'Tbilisi', 'Euro', 'Turkish lira', 'Euro'],
+                      'capital_city': ['Yerevan', 'Baku', 'Tbilisi', 'Pristina', 'Ankara', 'Vatican City'],
+                      'region': ['Eastern Europe', 'Eastern Europe', 'Eastern Europe', 'Southeast Europe', 'Southeast Europe', 'Southern Europe'],
+                      'gdp': [58497000000, 192146000000, 82210000000, 27918000000, 3613000000000, 16195272],
+                      'population': [3000756, 10353296, 3688647, 1761985, 85279553, 764],
+                      'democracy_type': ['Parlimentary democracy', 'Presidential republic', 'Parlimentary democracy', 'Parlimentary democracy', 'Presidential republic', 'Monarchy']}
+    
+    missing_countries_df = pd.DataFrame(missing_countries_data)
+    new_europe_data = pd.concat([europe_data, missing_countries_df], ignore_index=True)
+    new_europe_data = new_europe_data.sort_values(by='country', ignore_index=True)
+
+    return new_europe_data
 
 def intro_message():
     print("\n\033[1;4m\t\tWelcome to the European Travel and Information Guide\033[0m\n")
     print("\tHere will be a travel guide that details information regarding a country\n\t" + 
-          "and its capital city. In addition, the guide will provide the user with\n\t" +
+          "and its capital city. In addition, the guide shoulde provide you with\n\t" +
           "information regarding the countries':")
     print("\t\t- Cuisine" +
          "\n\t\t- Transportation" +
@@ -62,6 +90,7 @@ def main():
     print(european_data)
 
     while True:
+        break
         print_command_list()
         user_input = input("Please enter your next command: ")
 
