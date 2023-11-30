6fe191ab296027459ce421d543d112381aac697a
Mary Meier <mrose041102@gmail.com>
2023-11-30 17:52:22 -0500

    sql code to add tables

--- connect.py
+++ connect.py
@@ -1,21 +1,73 @@
 import sqlite3
+import csv
 
-def connect_travelSearch():
+rows = []
+
+def create_Country():
     # Connect to sqlite and 
     # Connect to tableSearch database
     connection = sqlite3.connect('travelSearch.db')
     # Cursor object
-    cursor = conn.cursor()
+    cursor = connection.cursor()
     cursor.execute('DROP TABLE IF EXISTS COUNTRY')
-    table = """CREATE TABLE Country (
-govt_struct VARCHAR(20), 
-language VARCHAR(20), 
-religion VARCHAR(20), 
-time_zone VARCHAR(30),
-name VARCHAR(50) NOT NULL, 
-PRIMARY KEY(name)
-);
- """
-    cursor.execute(table)
+    create_country_table = """CREATE TABLE Country (
+                            govt_struct VARCHAR(20), 
+                            language VARCHAR(20), 
+                            most_common_religion VARCHAR(20), 
+                            time_zone VARCHAR(30),
+                            name VARCHAR(50) NOT NULL, 
+                            PRIMARY KEY(name)
+                            );
+    """
+    cursor.execute(create_country_table)
+    file = open('Country_table.csv')
+    contents = csv.reader(file)
+
+    insert_records = "INSERT INTO Country (name, most_common_religion, language, govt_struct, time_zone) VALUES (?,?,?,?,?)"
+        
+    cursor.executemany(insert_records, contents)
+
+    select_all = "SELECT * FROM Country"
+    rows = cursor.execute(select_all).fetchall()
+
+    for r in rows:
+        print(r)
+
+    connection.commit()
     connection.close()
-    # conn.commit
+        
+def create_capital_city():
+    # Connect to sqlite and 
+    # Connect to tableSearch database
+    connection = sqlite3.connect('travelSearch.db')
+    # Cursor object
+    cursor = connection.cursor()
+    cursor.execute('DROP TABLE IF EXISTS CAPITAL_CITY')
+    create_capital_city_table = """CREATE TABLE Capital_City (
+                            capital_city VARCHAR(30) NOT NULL,
+                            population INT,
+                            capital_city_region VARCHAR(30),
+                            country VARCHAR(50),
+                            PRIMARY KEY(name),
+                            FOREIGN KEY (country) REFERENCES Country(name)
+                            );
+    """
+    cursor.execute(create_capital_city_table)
+    file = open('Capital_City_table.csv')
+    contents = csv.reader(file)
+
+    insert_records = "INSERT INTO Capital_City (country,capital_city,population,capital_city_region) VALUES (?,?,?,?)"
+        
+    cursor.executemany(insert_records, contents)
+
+    select_all = "SELECT * FROM Capital_City"
+    rows = cursor.execute(select_all).fetchall()
+
+    for r in rows:
+        print(r)
+
+    connection.commit()
+    connection.close()
+
+if __name__ == "__connect_travelSearch__":
+    connect_travelSearch()
